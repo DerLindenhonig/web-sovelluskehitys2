5f24c91f0d03f50db8c39e670fba28b04d6468cf
annarae <anna.raevskaia@metropolia.fi>
2021-12-07 16:25:13 +0200

    Quiz game korjaukset, score.

--- studyapp-frontend/src/components/Games.js
+++ studyapp-frontend/src/components/Games.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import cardService from '../services/cards'
+import Togglable from './Togglable'
 
 const Games = ({ blog }) => {
 
@@ -8,6 +9,13 @@ const Games = ({ blog }) => {
   }
 
   const [allCards, setAllCards] = useState([])
+  let [record, setRecord] = useState(null)
+  let [guestionText, setQuestionText] = useState('question')
+  let [answer1Text, setAnswer1Text] = useState('answer 1')
+  let [answer2Text, setAnswer2Text] = useState('answer 2')
+  let [answer3Text, setAnswer3Text] = useState('answer 3')
+  let [answer4Text, setAnswer4Text] = useState('answer 4')
+  let [randQ1, setRandQ1] = useState(null)
 
   useEffect(() => {
     cardService.getAll()
@@ -23,10 +31,6 @@ const Games = ({ blog }) => {
     }
   }
 
-  const StartQuizGame = () => {
-    changeAnswers()
-  }
-
   let arrayOfAnswers = []
   let question = ''
   let answer1 = ''
@@ -34,52 +38,64 @@ const Games = ({ blog }) => {
   let answer3 = ''
   let answer4 = ''
   let randQ = null
+  //let record = null
 
   const Quiz = () => {
+    console.log('randQ / Quiz ' + randQ1)
     return (
       <div>
-        <h2 id='question'>question</h2>
-        <button id='answer1' onClick={FirstAnswerSelected}>answer 1</button>
-        <button id='answer2' onClick={SecondAnswerSelected}>answer 2</button>
-        <button id='answer3' onClick={ThirdAnswerSelected}>answer 3</button>
-        <button id='answer4' onClick={FourthAnswerSelected}>answer 4</button>
+        <div>right answers: {record}</div>
+        <h2 id='question'>{guestionText}</h2>
+        <button id='answer1' onClick={FirstAnswerSelected}>{answer1Text}</button>
+        <button id='answer2' onClick={SecondAnswerSelected}>{answer2Text}</button>
+        <button id='answer3' onClick={ThirdAnswerSelected}>{answer3Text}</button>
+        <button id='answer4' onClick={FourthAnswerSelected}>{answer4Text}</button>
+        <br/>
+        <button id='next' onClick={changeAnswers}>next</button>
       </div>
     )
   }
 
   const FirstAnswerSelected = () => {
-    if(randQ === 0) {
+    if(randQ1 === 0) {
       console.log('RIGHT!')
-      changeAnswers()
+      record++
+      setRecord(record)
     } else {
-      console.log('WRONG!' + randQ)
+      console.log('WRONG!' + randQ1)
     }
     changeAnswers()
   }
 
   const SecondAnswerSelected = () => {
-    if(randQ === 1) {
+    if(randQ1 === 1) {
       console.log('RIGHT!')
+      record++
+      setRecord(record)
     } else {
-      console.log('WRONG!' + randQ)
+      console.log('WRONG!' + randQ1)
     }
     changeAnswers()
   }
 
   const ThirdAnswerSelected = () => {
-    if(randQ === 2) {
+    if(randQ1 === 2) {
       console.log('RIGHT!')
+      record++
+      setRecord(record)
     } else {
-      console.log('WRONG!' + randQ)
+      console.log('WRONG!' + randQ1)
     }
     changeAnswers()
   }
 
   const FourthAnswerSelected = () => {
-    if(randQ === 3) {
+    if(randQ1 === 3) {
       console.log('RIGHT!')
+      record++
+      setRecord(record)
     } else {
-      console.log('WRONG!' + randQ)
+      console.log('WRONG!' + randQ1)
     }
     changeAnswers()
   }
@@ -90,14 +106,19 @@ const Games = ({ blog }) => {
     arrayOfAnswers = []
     while(arrayOfAnswers.length < 4){
       randA = Math.floor(Math.random() * cards.length)
-      if(arrayOfAnswers.filter(a => a === cards[randA])) {
+      if(!arrayOfAnswers.includes(cards[randA])) {
         arrayOfAnswers.push(cards[randA])
-        console.log(cards[randA])
+        console.log('cards[randA] ' + cards[randA].id)
       }
     }
+    console.log('arrayOfAnswers 0: ' + arrayOfAnswers[0].id + ' 1: ' + arrayOfAnswers[1].id + ' 2: ' + arrayOfAnswers[2].id + ' 3: ' + arrayOfAnswers[3].id )
 
     randQ = Math.floor(Math.random() * arrayOfAnswers.length)
     const randQuestion = arrayOfAnswers[randQ]
+
+    console.log('randQ ' + randQ)
+
+    setRandQ1(randQ)
 
     question = randQuestion.word
     answer1 = arrayOfAnswers[0].translate
@@ -115,13 +136,20 @@ const Games = ({ blog }) => {
     btn4.innerHTML = answer4
     let quest = document.getElementById('question')
     quest.innerHTML = question
+
+    setQuestionText(question)
+    setAnswer1Text(answer1)
+    setAnswer2Text(answer2)
+    setAnswer3Text(answer3)
+    setAnswer4Text(answer4)
   }
 
   return (
     <div>
       <h1>Games</h1>
-      <div><button id='start' onClick={StartQuizGame}>start</button></div>
-      <Quiz/>
+      <Togglable buttonLabel='start'>
+        <Quiz/>
+      </Togglable>
     </div>
   )
 }
